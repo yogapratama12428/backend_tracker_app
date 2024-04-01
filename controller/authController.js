d9bcfbf1fc1a6df9fa25fbfdbee869d136f90782
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2024-04-01 13:46:07 +0700

    R 4.1.2
    fix cookie error on logout

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from 'bcryptjs'
-import jsonwebtoken from 'jsonwebtoken'
+import bcrypt from "bcryptjs";
+import jsonwebtoken from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
@@ -10,124 +10,126 @@ export const register = async (req, res) => {
   try {
     const isUser = await prisma.user.findUnique({
       where: {
-        email
-      }
-    })
+        email,
+      },
+    });
 
     if (isUser) {
       return res.status(400).json({
-        message: "User already exists"
-      })
+        message: "User already exists",
+      });
     }
 
-    const hash_password = await bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+    const hash_password = await bcrypt.hashSync(
+      password,
+      bcrypt.genSaltSync(10)
+    );
 
-    const access_token = jsonwebtoken.sign({
-      email: email
-    }, process.env.JWT_SECRET, {
-      expiresIn: 1000 * 60 * 60 * 24
-    })
+    const access_token = jsonwebtoken.sign(
+      {
+        email: email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 1000 * 60 * 60 * 24,
+      }
+    );
 
     const createdata = await prisma.user.create({
       data: {
         email,
         password: hash_password,
-        access_token
-      }
-    })
+        access_token,
+      },
+    });
 
-    console.log(createdata)
+    console.log(createdata);
 
-    return res
-      .status(200)
-      .json({ msg: 'User registered successfully' });
-
+    return res.status(200).json({ msg: "User registered successfully" });
   } catch (error) {
-    return res.status(401).json(error.message)
+    return res.status(401).json(error.message);
   }
 };
 
 export const login = async (req, res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
   try {
     const isUser = await prisma.user.findUnique({
       where: {
-        email
-      }
-    })
+        email,
+      },
+    });
 
-    if (!isUser) return res.status(404).json({
-      msg: 'User not found',
-    })
+    if (!isUser)
+      return res.status(404).json({
+        msg: "User not found",
+      });
 
     const isMatch = await bcrypt.compare(password, isUser.password);
 
-    if (!isMatch) return res.status(401).json({
-      msg: 'Invalid password',
-    })
+    if (!isMatch)
+      return res.status(401).json({
+        msg: "Invalid password",
+      });
 
-    const token = jsonwebtoken.sign({
-      email: isUser.email
-    }, process.env.JWT_SECRET, {
-      expiresIn: 1000 * 60 * 60 * 24
-    })
+    const token = jsonwebtoken.sign(
+      {
+        email: isUser.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 1000 * 60 * 60 * 24,
+      }
+    );
 
     await prisma.user.update({
       where: {
-        email
+        email,
       },
       data: {
-        access_token: token
-      }
-    })
+        access_token: token,
+      },
+    });
 
-    res.cookie('token', token, {
+    res.cookie("token", token, {
       expires: new Date(Date.now() + 86400000),
       httpOnly: false, // if true -> only browser
       withCredentials: true,
       secure: true, // only works on https
       sameSite: "None", // only works on production
-    })
+    });
 
-    return res
-      .status(200)
-      .json({
-        msg: 'User logged in successfully',
-        data: isUser
-      })
+    return res.status(200).json({
+      msg: "User logged in successfully",
+      data: isUser,
+    });
   } catch (error) {
-    return res
-      .status(500).json({
-        msg: error.message
-      })
+    return res.status(500).json({
+      msg: error.message,
+    });
   }
-}
+};
 
 export const logout = async (req, res) => {
-
   try {
-
     const access_token = req.cookies.token;
 
-    console.log(access_token)
+    console.log(access_token);
 
     await prisma.user.updateMany({
       where: {
-        access_token
+        access_token,
       },
       data: {
-        access_token: "USER LOGOUT"
-      }
-    })
+        access_token: "USER LOGOUT",
+      },
+    });
 
     res.clearCookie("token");
     res.send({ success: true });
   } catch (error) {
-    return res
-      .status(500).json({
-        msg: error.message
-      })
+    return res.status(500).json({
+      msg: error.message,
+    });
   }
-
-
-}
+};
